342820d58527689b1b866ce48ac74f253e4bc8d7
Cong Hoan Ngo <conghoan.0406@gmail.com>
2018-07-13 14:18:27 +0700

    Chapter 5: Filling in the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,6 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title page_title = ""
     base_title = t(".base_title")
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
