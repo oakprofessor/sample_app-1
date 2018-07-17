3e3f2ebf96e6154a8c36d36d325a227544a75c18
Cong Hoan Ngo <conghoan.0406@gmail.com>
2018-07-17 14:50:12 +0700

    Chapter 9: Advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    @user = User.find_by email: params[:session][:email].downcase
+    if @user&.authenticate params[:session][:password]
+      log_in @user
+      remember_checker
+      redirect_to @user
     else
       flash.now[:danger] = t "controllers.sessions_controller.invalid"
       render :new
@@ -13,7 +14,12 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+  def remember_checker
+    params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_034742) do
+ActiveRecord::Schema.define(version: 2018_07_17_063836) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_10_034742) do
     t.datetime "create_at"
     t.datetime "updated_at"
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
