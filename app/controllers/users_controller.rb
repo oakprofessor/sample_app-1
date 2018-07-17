d035ec2691916c5fab8afcff685fa90e093059af
Cong Hoan Ngo <conghoan.0406@gmail.com>
2018-07-17 17:11:51 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def show
-    @user = User.find_by id: params[:id]
     return if @user
     flash[:danger] = t ".user_not_found"
     redirect_to root_path
@@ -21,10 +29,45 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".edit_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    flash[:success] = t ".delete_success" if @user.destroy
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".login_check"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
   end
 end
